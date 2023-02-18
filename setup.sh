bb7d115932dcb7b053bb06253cf001e023fe092c
Nandigam Charan <57171014+NandigamCharan@users.noreply.github.com>
2023-02-19 04:05:46 +0530

    initial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# install XCODE Command Line Tools
+xcode-select --install
+
+# install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone
+
+# install brew
